e24c11ba29ece694879e2ae9d0429280f1b91158
alaa bel <alaaeddine.benlagha@esprit.tn>
2022-02-12 03:56:18 +0100

    First commit

---
+++ src/main/java/com/pidev/WellJobApplication.java
@@ -0,0 +1,13 @@
+package com.pidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pidev/WellJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
